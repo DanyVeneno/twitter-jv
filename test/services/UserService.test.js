aaf8fad975f8a8f60be770490c2a4df53a9b1058
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-04-22 12:07:15 -0500

    3 Se corrio el test y dio fallo sigiente paso editar el documento correspondiente

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -5,10 +5,10 @@ describe("Test for UserService", () =>{
 	test("1: Create a new user using the UserService",()=>{
 
 
-		const user = UserService.crate(1,"daniel","danny")
+		const user = UserService.create(1,"daniel","danny")
 		expect(user.username).toBe("daniekl")
 		expect(user.name).toBe("danny")
-		expect(user.id).toBe)(1)
+		expect(user.id).toBe(1)
 		expect(user.bio).not.toBeUndefined()
 
 
