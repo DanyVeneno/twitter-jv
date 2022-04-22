dc5e2a9f8589943ec6a76025fe9872f012bb5f0c
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-04-22 13:10:31 -0500

    5 se creo el tercer requerimieto actualizar el usename se corrio el test con falla

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -13,4 +13,22 @@ describe("Test for UserService", () =>{
 
 
 	})
+
+	test("2: Get all user data in a list", () =>{
+
+		const user = UserService.create(1,"daniel","danny")
+		const userInfoList =  UserService.getInfo(user)
+		expect(userInfoInList[0]).toBe(1)
+		expect(userInfoInList[1]).toBe("daniel")
+		expect(userInfoInList[2]).toBe("danny")
+		expect(userInfoInList[3]).toBe("sin bio")
+	})
+
+	test("3: Update username",()=>{
+
+		const user = UserService.create(1,"daniel","danny")
+		UserService.updateUserUsername(user,"danny veneno")
+		expect(user.username).toBe("danny veneno")
+
+	})
 })
