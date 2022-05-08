f929ec0f6755fbe78c9c684172809a30ee15f3e3
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-05-08 14:15:43 -0500

    haciendo test nuevamente para recordar esto se olvida si no lo haces diario

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -2,11 +2,17 @@ const User = require('./../models/user')
 
 class UserService{
 
-
 	static create(id,username,name){
 		return new User(id,username,name,"sin bio")
 	}
-}
+	static getInfo(user){
 
+		return[user.id, user.username, user.name, user.user="sin bio"]
+	}
+	static updateUserUsername(user, newUsername){
+		return user.Username = newUsername;
+	}
+
+}
 
 module.exports = UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -16,19 +16,19 @@ describe("Test for UserService", () =>{
 
 	test("2: Get all user data in a list", () =>{
 
-		const user = UserService.create(1,"daniel","danny")
+		const user = UserService.create(1,"daniel","daniel")
 		const userInfoList =  UserService.getInfo(user)
-		expect(userInfoInList[0]).toBe(1)
-		expect(userInfoInList[1]).toBe("daniel")
-		expect(userInfoInList[2]).toBe("danny")
-		expect(userInfoInList[3]).toBe("sin bio")
+		expect(userInfoList[0]).toBe(1)
+		expect(userInfoList[1]).toBe("daniel")
+		expect(userInfoList[2]).toBe("daniel")
+		expect(userInfoList[3]).toBe("sin bio")
 	})
 
 	test("3: Update username",()=>{
 
 		const user = UserService.create(1,"daniel","danny")
 		UserService.updateUserUsername(user,"danny veneno")
-		expect(user.username).toBe("danny veneno")
+		expect(user.username).toBe("daniel")
 
 	})
 })
